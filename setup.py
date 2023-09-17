a67d8db20010a7f096aceb77e58de2fb3af23208
Patrick Morton <patrick.morton.-nd@disney.com>
2023-09-17 18:52:49 -0400

    update for testpypi

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setuptools.setup(name='weather_api',
                  author_email = 'jarhead8795@gmail.com',
                  description = 'Location and weather forecast package',
                  long_description='Location and weather forecast package',
-                 long_description_content_type = 'text/x-rst,'
                  packages=setuptools.find_packages(),
                  classifiers=["Programming Language :: Python :: 3",
                               "License :: OSI Approved :: MIT License",

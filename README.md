b278af5604e01c9ef00e056c84169bb212bcaa41
Patrick Morton <patrick.morton.-nd@disney.com>
2023-09-17 20:30:09 -0400

    make api key a parameter

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # weather_api
+
+Provides functions to get location data from zip code and get weather forecast for the area specified by the location data.
+
+This package requires that you have a valid API key to provide the Weather API. 
+An API key can be obtained by visiting this site. https://openweathermap.org/current
+
+Once you have obtained a valid API key you must create a file call `config.py` and place the following line inside the file.
+`WEATHER_API_KEY = '<Your API KEY'>`
--- weather_api/weather_api.py
+++ weather_api/weather_api.py
@@ -1,13 +1,12 @@
 import requests
 
-from config import WEATHER_API_KEY
 
 WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
 COORD_URL = "http://api.openweathermap.org/geo/1.0/zip"
 HEADERS_JSON  = {"Accept": "application/json","Content-Type": "application/json"}
   
   
-def get_location(zip_code: str, country_code: str = None) -> list:
+def get_location(zip_code: str, country_code: str = None, api_key=None) -> list:
     """
     Get the location coordinates from the given zip code and country code
     
@@ -17,7 +16,7 @@ def get_location(zip_code: str, country_code: str = None) -> list:
     Returns:
         Location data (dict)
     """
-    url = f"{COORD_URL}?zip={zip_code}&appid={WEATHER_API_KEY}"
+    url = f"{COORD_URL}?zip={zip_code}&appid={api_key}"
     
     if country_code:
         url = url + "&country_code={country_code}"
@@ -28,7 +27,7 @@ def get_location(zip_code: str, country_code: str = None) -> list:
     return response.json()
 
 
-def get_weather_forecast(lat:str = None, lon: str = None, units: str = 'imperial') -> dict:
+def get_weather_forecast(lat:str = None, lon: str = None, units: str = 'imperial', api_key=None) -> dict:
     """
     Gets the weather forecast for the given coordinates
     
@@ -39,7 +38,7 @@ def get_weather_forecast(lat:str = None, lon: str = None, units: str = 'imperial
     Returns:
         Weather data (dict)
     """
-    url = f"{WEATHER_URL}?lat={lat}&lon={lon}&units={units}&appid={WEATHER_API_KEY}"
+    url = f"{WEATHER_URL}?lat={lat}&lon={lon}&units={units}&appid={api_key}"
 
     response = requests.get(url, headers = HEADERS_JSON)
     response.raise_for_status()
